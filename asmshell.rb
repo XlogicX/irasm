4de97446cde9aa00b6c24008f88274a08a611eca
XlogicX <no.axiom@gmail.com>
2020-01-02 13:34:23 -0500

    Support 16bit mode with -b
    This is to support usefulness with exploring machine code differences when trying to optimize boot sector programming

--- asmshell.rb
+++ asmshell.rb
@@ -1,20 +1,20 @@
 #!/usr/bin/ruby
 
-def main
+def main(bits)
 	while 1 do
 		print 'asmshell > '
 		asm = gets.chomp
 		if /(quit|(?<!s)exit)/i.match(asm)
 			exit
 		end 
-
-		nasm(asm) 
+		nasm(asm,bits) 
 	end
 end
 
-def nasm (data)
+def nasm (data,bits)
 	file = File.open("tmp.asm", "w")
-	file.write("[BITS 32]\n" + data + "\n")
+	if bits == 32 then file.write("[BITS 32]\n" + data + "\n")
+	else file.write("[BITS 16]\n" + data + "\n") end
 	file.close
 	cmd = 'nasm -f bin tmp.asm'
 	system(cmd)
@@ -24,4 +24,8 @@ def nasm (data)
 	system('rm tmp tmp.asm')
 end
 
-main
+bits = 32
+if ARGV[0] == '-b' then bits = 16 end
+ARGV.clear
+
+main(bits)
